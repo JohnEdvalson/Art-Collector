a5c57e617bee7723d6bf16c06e1bdb2e3d6333df
johnedvalson <califaxthecool@gmail.com>
2022-07-04 12:27:41 -0600

    build to deploy

---
+++ build/precache-manifest.2563719f80ae6ddb73acdd077ec0d91f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2c225ba3e213bc6dfb4cbf7a832f3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb3a1cab9e6012fcc7",
+    "url": "/static/js/2.6c99d600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c99d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1232672a9dcbb727e3",
+    "url": "/static/js/main.19a7fb84.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
